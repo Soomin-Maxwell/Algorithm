1fd71283caf7046ab75f09846bb686df452d649e
Soomin Lee <64841056+Soomin-Maxwell@users.noreply.github.com>
2021-04-28 23:43:32 +0900

    Add files via upload

---
+++ Sort_programmers_1.py
@@ -0,0 +1,21 @@
+
+def solution(array, commands):
+    answer = []
+
+    for command in commands : 
+        i = command[0]
+        j = command[1]
+        k = command[2]
+
+        myList_1 = array[i-1:j]
+        myList_1.sort()
+        answer.append(myList_1[k-1])
+
+    return answer
+
+
+'''
+
+Source : https://programmers.co.kr/learn/courses/30/lessons/42748
+
+'''
